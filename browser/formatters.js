fadc9909a5a3c7622f5f14cb85919c2356ce4f96
kenny fung <fungtingkwan@gmail.com>
2022-01-03 20:44:23 +0000

    better range formatting

--- browser/formatters.js
+++ browser/formatters.js
@@ -1,6 +1,6 @@
 const numberFormat = Intl.NumberFormat('en-US').format;
 const currencyFormat = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format;
-const rangeFormat = (str) => str.split('-').filter(Boolean).map(n => currencyFormat(Number(n))).join('-');
+const rangeFormat = (str) => str.split('-').filter(Boolean).map(n => currencyFormat(Number(n))).join(' - ');
 
 module.exports = {
   numberFormat,
