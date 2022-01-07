693b3bd30a6e9f763ab606076cfb1643dce24115
kenny fung <fungtingkwan@gmail.com>
2022-01-07 21:44:38 +0000

    add news and tabs

--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,7 @@
 const { renderChart } = require('./browser/chart');
 const { renderFinancials } = require('./browser/financials');
 const { renderExchangeData } = require('./browser/exchanges');
+const { getNews } = require('./browser/teslaratiFeed');
 const { ipcRenderer } = require('electron');
 
 const dayjs = require('dayjs');
@@ -11,6 +12,16 @@ dayjs.extend(utcPlugin)
 dayjs.extend(timezonePlugin)
 
 let chartInstance;
+
+document.getElementById('news-tab').onclick = () => {
+  document.getElementById('stock').style.display = 'none';
+  document.getElementById('articles').style.display = 'block';
+}
+
+document.getElementById('stock-tab').onclick = () => {
+  document.getElementById('articles').style.display = 'none';
+  document.getElementById('stock').style.display = 'block';
+}
 
 document.getElementById('resume').onclick = () => {
   ipcRenderer.send('resume-ws')
@@ -28,6 +39,7 @@ ipcRenderer.on('render-data', async () => {
   await renderExchangeData();
   chartInstance = await renderChart();
   await renderFinancials();
+  await getNews();
 })
 
 ipcRenderer.on('window-hide', () => {
--- browser/teslaratiFeed.js
+++ browser/teslaratiFeed.js
@@ -2,38 +2,44 @@ const axios = require('axios');
 const dayjs = require('dayjs');
 const { shell } = require('electron');
 
-axios
-  .get('https://www.teslarati.com/feed/')
-  .then(response => response.data)
-  .then(data => new DOMParser().parseFromString(data, "text/xml"))
-  .then(doc => {
-    const items = doc.querySelectorAll('item');
+const getNews = async () => {
+  return axios
+    .get('https://www.teslarati.com/feed/')
+    .then(response => response.data)
+    .then(data => new DOMParser().parseFromString(data, "text/xml"))
+    .then(doc => {
+      const items = doc.querySelectorAll('item');
 
-    const container = document.createElement('div');
+      const container = document.createElement('div');
 
-    Array.from(items).map(item => {
-      const title = item.querySelector('title').textContent;
-      const link = item.querySelector('link').textContent;
-      const description = item.querySelector('description').textContent;
-      const pubDate = item.querySelector('pubDate').textContent; //Wed, 05 Jan 2022 10:20:06 +0000
+      Array.from(items).map(item => {
+        const title = item.querySelector('title').textContent;
+        const link = item.querySelector('link').textContent;
+        const description = item.querySelector('description').textContent;
+        const pubDate = item.querySelector('pubDate').textContent; //Wed, 05 Jan 2022 10:20:06 +0000
 
-      const temp = document.createElement('div')
-      temp.innerHTML = description;
+        const temp = document.createElement('div')
+        temp.innerHTML = description;
 
-      const html = `
-        <article>
-          <h2 class="article-title">${title}</h2>
-          <span class="article-pubdate">${dayjs(pubDate).format('MMM D, YYYY')}</span>
-          <p class="artile-description">${temp.querySelectorAll('p')[0].textContent}</p>
-        </article>
-      `;
+        const html = `
+          <article>
+            <h2 class="article-title">${title}</h2>
+            <span class="article-pubdate">${dayjs(pubDate).format('MMM D, YYYY')}</span>
+            <p class="artile-description">${temp.querySelectorAll('p')[0].textContent}</p>
+          </article>
+        `;
 
-      const dom = new DOMParser().parseFromString(html, 'text/html');
+        const dom = new DOMParser().parseFromString(html, 'text/html');
 
-      dom.querySelector('article').onclick = () => shell.openExternal(link);
+        dom.querySelector('article').onclick = () => shell.openExternal(link);
 
-      container.append(dom.querySelector('article'));
+        container.append(dom.querySelector('article'));
+      })
+
+      document.getElementById('articles').replaceChildren(container);
     })
+}
 
-    document.body.append(container);
-  })
+module.exports = {
+  getNews,
+}
